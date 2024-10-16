7799a1965cc7ea60463f9e6a4c6d7a520a0bff31
Igor Borozdin <igor@borozd.in>
2024-10-16 23:24:57 +0300

    Add Ingredient class unit tests

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -6,18 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class BunTest {
 
-    private String name = "Название булки";
-    private float price = 12.5f;
+    private String expectedName = "Название булки";
+    private float expectedPrice = 12.5f;
 
     @Test //Проверяет, что метод getName() возвращает name установленное для объекта класса Bun
     public void getNameBunTest() {
-        Bun bun = new Bun(name, price);
-        assertEquals(name, bun.getName());
+        Bun bun = new Bun(expectedName, expectedPrice);
+        assertEquals(expectedName, bun.getName());
     }
 
     @Test //Проверяет, что метод getPrice() возвращает price установленное для объекта класса Bun
     public void getPriceBunTest() {
-        Bun bun = new Bun(name, price);
-        assertEquals(price, bun.getPrice(), 0.001);
+        Bun bun = new Bun(expectedName, expectedPrice);
+        assertEquals(expectedPrice, bun.getPrice(), 0.001);
     }
 }
