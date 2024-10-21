3bac56ecd303ee7684e9a60859a54820e8f3bfb1
Igor Borozdin <igor@borozd.in>
2024-10-22 00:37:07 +0300

    Refactor ingredients tests

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -8,22 +8,20 @@ public class IngredientTest {
     private String expectedName = "Название ингридиента";
     private float expectedPrice = 12.5f;
     IngredientType expectedType = IngredientType.SAUCE;
+    Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
 
     @Test //Проверяет, что метод getName() возвращает name установленное для объекта класса Ingredient
     public void getNameIngredientTest() {
-        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
         assertEquals(expectedName, ingredient.getName());
     }
 
     @Test //Проверяет, что метод getPrice() возвращает price установленное для объекта класса Ingredient
     public void getPriceIngredientTest() {
-        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
         assertEquals(expectedPrice, ingredient.getPrice(), 0.001);
     }
 
     @Test //Проверяет, что метод getType() возвращает IngredientType установленный для объекта класса Ingredient
     public void getTypeIngredientTest() {
-        Ingredient ingredient = new Ingredient(expectedType, expectedName, expectedPrice);
         assertEquals(expectedType, ingredient.getType());
     }
 
