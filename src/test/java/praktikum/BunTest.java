ad2b797787a709ccb73b7734b23f9ffae387f19f
Igor Borozdin <igor@borozd.in>
2024-10-22 00:36:35 +0300

    Add parametrized tests

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,23 +1,40 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import static org.junit.Assert.assertEquals;
 
-
+@RunWith(Parameterized.class)
 public class BunTest {
 
-    private String expectedName = "Название булки";
-    private float expectedPrice = 12.5f;
+    private final String name;
+    private final float price;
+
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][] {
+                {"white bun", 0},
+                {"black bun", 900},
+                {"", 800.123f},
+        };
+    }
 
     @Test //Проверяет, что метод getName() возвращает name установленное для объекта класса Bun
     public void getNameBunTest() {
-        Bun bun = new Bun(expectedName, expectedPrice);
-        assertEquals(expectedName, bun.getName());
+        Bun bun = new Bun(name, price);
+        assertEquals(name, bun.getName());
     }
 
     @Test //Проверяет, что метод getPrice() возвращает price установленное для объекта класса Bun
     public void getPriceBunTest() {
-        Bun bun = new Bun(expectedName, expectedPrice);
-        assertEquals(expectedPrice, bun.getPrice(), 0.001);
+        Bun bun = new Bun(name, price);
+        assertEquals(price, bun.getPrice(), 0.001);
     }
 }
